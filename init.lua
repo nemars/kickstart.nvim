b9463988350a950c982f7ac7d04210c019bc7d51
nemars <35416700+nemars@users.noreply.github.com>
2025-04-21 18:28:14 +0200

    Set global variables

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
+-- [[ Setting global variables ]]
+-- NOTE: See help for global variables with ':help <variable-name>'
+
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
